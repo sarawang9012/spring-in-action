ab5bd4bb3e9d64e3fbc66774a0d48cf2102166c1
sarawang9012 <625504790@qq.com>
2023-12-15 10:44:42 +0800

    chapter 1

---
+++ src/test/java/com/sia/tacos/HomeControllerTest.java
@@ -0,0 +1,42 @@
+package com.sia.tacos;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * ClassName: HomeControllerTest
+ * Package: com.sia.tacos
+ * Description:
+ *
+ * @Author Sara Wang
+ * @Create 2023/12/13 10:45
+ * @Version 1.0
+ */
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to...")
+                ));
+
+    }
+
+
+}
